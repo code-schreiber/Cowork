bb5fe11f36e65c99f781792181c964b75ccb5594
code-schreiber <code-schreiber@users.noreply.github.com>
2018-07-09 12:32:05 +0200

    Adds deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXPECTED_TRAVIS_REPO_SLUG="code-schreiber/AndroidStarterKit"
+EXPECTED_TRAVIS_REPO_SLUG="code-schreiber/Cowork"
 EXPECTED_TRAVIS_BRANCH="master"
 
 # Script should bail on first error
