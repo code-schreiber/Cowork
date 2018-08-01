56e3d7fd3cd775d9e6c5aa09b07960ddefed73ac
code-schreiber <code-schreiber@users.noreply.github.com>
2018-08-01 11:43:53 +0200

    Refactoring

--- base_repository/src/main/java/com/toolslab/cowork/base_repository/SpaceRepository.kt
+++ base_repository/src/main/java/com/toolslab/cowork/base_repository/SpaceRepository.kt
@@ -27,13 +27,16 @@ class SpaceRepository @Inject constructor() {
     }
 
     private fun listSpaces(country: String, space: String, city: String): Single<List<Space>> {
-        val single = if (tokenRepository.getToken().isValid()) {
+        return if (tokenRepository.isTokenValid()) {
             listSpacesAlreadyAuthenticated(country, space, city)
+                    .onErrorResumeNext {
+                        errorHandler.handle(it)
+                    }
         } else {
             listSpacesAuthenticatingFirst(country, space, city)
-        }
-        return single.onErrorResumeNext {
-            errorHandler.handle(it)
+                    .onErrorResumeNext {
+                        errorHandler.handle(it)
+                    }
         }
     }
 
@@ -45,7 +48,7 @@ class SpaceRepository @Inject constructor() {
                     }
 
     private fun listSpacesAlreadyAuthenticated(country: String, space: String, city: String) =
-            coworkingMapApi.listSpacesAlreadyAuthenticated(tokenRepository.getToken().token, country, space, city)
+            coworkingMapApi.listSpacesAlreadyAuthenticated(tokenRepository.getToken(), country, space, city)
                     .onErrorResumeNext {
                         handleExpiredToken(it, country, city, space)
                     }
@@ -53,7 +56,7 @@ class SpaceRepository @Inject constructor() {
     private fun handleExpiredToken(throwable: Throwable, country: String, city: String, space: String): Single<List<Space>> =
             if (coworkingMapApi.isTokenExpired(throwable)) {
                 // TODO for debuging expired token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvY293b3JraW5nbWFwLm9yZyIsImlhdCI6MTUzMTQwMTk3NCwibmJmIjoxNTMxNDAxOTc0LCJleHAiOjE1MzIwMDY3NzQsImRhdGEiOnsidXNlciI6eyJpZCI6IjI2NDYifX19.uVwF7RdTmeB8ANmt0rTCTzXMCN7zEq5znYC-wuorRQE"
-                Logger.getGlobal().log(Level.FINE, "Token was expired") // TODO does this show up in logcat?
+                Logger.getGlobal().log(Level.FINE, "Token has expired") // TODO does this show up in logcat?
                 tokenRepository.invalidateToken()
                 listSpaces(city, space, country)
             } else {
--- base_repository/src/main/java/com/toolslab/cowork/base_repository/TokenRepository.kt
+++ base_repository/src/main/java/com/toolslab/cowork/base_repository/TokenRepository.kt
@@ -5,11 +5,9 @@ import javax.inject.Inject
 
 class TokenRepository @Inject constructor() {
 
-    internal val cachedToken = Token(
-            //"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvY293b3JraW5nbWFwLm9yZyIsImlhdCI6MTUzMTQwMTk3NCwibmJmIjoxNTMxNDAxOTc0LCJleHAiOjE1MzIwMDY3NzQsImRhdGEiOnsidXNlciI6eyJpZCI6IjI2NDYifX19.uVwF7RdTmeB8ANmt0rTCTzXMCN7zEq5znYC-wuorRQE"
-    )
+    internal val cachedToken = Token()
 
-    fun getToken() = cachedToken
+    fun getToken() = cachedToken.token
 
     fun saveToken(token: String) {
         cachedToken.token = token // TODO persist token in database
@@ -19,4 +17,5 @@ class TokenRepository @Inject constructor() {
         cachedToken.invalidate()
     }
 
+    fun isTokenValid() = cachedToken.isValid()
 }
