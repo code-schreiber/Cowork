0bb96534e17cdda5954010d9c52cfc22eacecb93
code-schreiber <code-schreiber@users.noreply.github.com>
2018-08-01 15:16:24 +0200

    Adds tests

--- base_network/src/test/java/com/toolslab/base_network/CoworkingMapApiTest.kt
+++ base_network/src/test/java/com/toolslab/base_network/CoworkingMapApiTest.kt
@@ -1,20 +1,21 @@
 package com.toolslab.base_network
 
-import com.nhaarman.mockito_kotlin.*
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
 import com.toolslab.cowork.base_network.CoworkingMapApi
 import com.toolslab.cowork.base_network.CoworkingMapService
-import com.toolslab.cowork.base_network.model.Jwt
+import com.toolslab.cowork.base_network.model.*
 import com.toolslab.cowork.base_network.model.Map
-import com.toolslab.cowork.base_network.model.Space
 import io.reactivex.Single
 import org.amshove.kluent.shouldEqual
 import org.junit.Before
 import org.junit.Test
+import retrofit2.HttpException
+import java.net.HttpURLConnection
 
 class CoworkingMapApiTest {
 
-    private val validToken = "a valid token"
-    private val invalidToken = "an invalid token"
+    private val token = "a token"
     private val country = "a country"
     private val city = "a city"
     private val space = "a space"
@@ -23,116 +24,69 @@ class CoworkingMapApiTest {
     private val space1 = Space(city, Map("address1", "lat1", "lng1"), "space1", "slug1")
     private val space2 = Space(city, Map("address2", "lat2", "lng2"), "space2", "slug2")
     private val spaces = listOf(space1, space2)
-    private val exception = Exception("an exception")
-    private val handledException = Exception("a handled exception")
-    private val jwt = Jwt(validToken, "", "", "", 0)
 
+    private val mockThrowable: HttpException = mock()
     private val mockCoworkingMapService: CoworkingMapService = mock()
-    private val mockHttpErrorHandler: HttpErrorHandler = mock()
 
     private val underTest = CoworkingMapApi()
 
     @Before
     fun setUp() {
         underTest.coworkingMapService = mockCoworkingMapService
-        underTest.httpErrorHandler = mockHttpErrorHandler
-//        underTest.credentials = Credentials(user, password, validToken)
 
-        val validBearerToken = underTest.createTokenForRequest(validToken)
-        val invalidBearerToken = underTest.createTokenForRequest(invalidToken)
-        val spacesSingle = Single.just(spaces)
-        val errorSingle: Single<List<Space>> = Single.error(exception)
-        whenever(mockCoworkingMapService.listSpaces(validBearerToken, country)).thenReturn(spacesSingle)
-        whenever(mockCoworkingMapService.listSpaces(validBearerToken, country, city)).thenReturn(spacesSingle)
+        val validBearerToken = underTest.createTokenForRequest(token)
+        whenever(mockCoworkingMapService.listSpaces(validBearerToken, country)).thenReturn(Single.just(spaces))
+        whenever(mockCoworkingMapService.listSpaces(validBearerToken, country, city)).thenReturn(Single.just(spaces))
         whenever(mockCoworkingMapService.listSpaces(validBearerToken, country, city, space)).thenReturn(Single.just(space1))
-        whenever(mockCoworkingMapService.listSpaces(eq(invalidBearerToken), any())).thenReturn(errorSingle)
-        whenever(mockCoworkingMapService.listSpaces(eq(invalidBearerToken), any(), any())).thenReturn(errorSingle)
-        whenever(mockCoworkingMapService.listSpaces(eq(invalidBearerToken), any(), any(), any())).thenReturn(Single.just(space1))
     }
 
     @Test
-    fun listSpaces() {
-        val spacesInCountry = underTest.listSpaces("", "", country).blockingGet()
-        val spacesInCity = underTest.listSpaces(city, "", country).blockingGet()
-        val spaces = underTest.listSpaces(city, space, country).blockingGet()
+    fun getJwt() {
+        val expectedJwt = Jwt("", "", "", "", 12)
+        whenever(mockCoworkingMapService.getJwt(user, password)).thenReturn(Single.just(expectedJwt))
+
+        val jwt = underTest.getJwt(user, password).blockingGet()
+
+        jwt shouldEqual expectedJwt
+    }
+
+    @Test
+    fun listSpacesAlreadyAuthenticated() {
+        val spacesInCountry = underTest.listSpacesAlreadyAuthenticated(token, country, "", "").blockingGet()
+        val spacesInCity = underTest.listSpacesAlreadyAuthenticated(token, country, city, "").blockingGet()
+        val space = underTest.listSpacesAlreadyAuthenticated(token, country, city, space).blockingGet()
 
         spacesInCountry shouldEqual spaces
         spacesInCity shouldEqual spaces
-        spaces shouldEqual spaces
+        space shouldEqual listOf(space1)
     }
 
     @Test
-    fun listSpacesInCountryWithInvalidToken() {
-        underTest.credentials.token = invalidToken
-        whenever(mockHttpErrorHandler.handle<Space>(exception)).thenReturn(Single.error(handledException))
-        whenever(mockHttpErrorHandler.isTokenExpired(exception)).thenReturn(false)
+    fun validate() {
+        val expectedValidation = Validation("", Data(12))
+        whenever(mockCoworkingMapService.validate(token)).thenReturn(Single.just(expectedValidation))
 
-        val testObserverForCountry = underTest.listSpaces("", "", country).test()
-        val testObserverForCity = underTest.listSpaces(city, "", country).test()
-        val testObserverForSpace = underTest.listSpaces(city, space, country).test()
-        testObserverForCountry.awaitTerminalEvent()
-        testObserverForCity.awaitTerminalEvent()
-        testObserverForSpace.awaitTerminalEvent()
+        val validation = underTest.validate(token).blockingGet()
 
-        testObserverForCountry.apply {
-            valueCount() shouldEqual 0
-            errorCount() shouldEqual 1
-            errors()[0] shouldEqual handledException
-        }
-        testObserverForCity.apply {
-            valueCount() shouldEqual 0
-            errorCount() shouldEqual 1
-            errors()[0] shouldEqual handledException
-        }
-        testObserverForSpace.apply {
-            valueCount() shouldEqual 0
-            errorCount() shouldEqual 1
-            errors()[0] shouldEqual handledException
-        }
+        validation shouldEqual expectedValidation
     }
 
     @Test
-    fun listSpacesAuthenticatingFirst() {
-        underTest.credentials.token = ""
-        whenever(mockCoworkingMapService.getJwt(user, password)).thenReturn(Single.just(jwt))
+    fun isTokenExpired() {
+        whenever(mockThrowable.code()).thenReturn(HttpURLConnection.HTTP_FORBIDDEN)
 
-        val result = underTest.listSpaces(city, space, country).blockingGet()
+        val isTokenExpired = underTest.isTokenExpired(mockThrowable)
 
-        result shouldEqual spaces
-        underTest.credentials.token shouldEqual validToken
+        isTokenExpired shouldEqual true
     }
 
     @Test
-    fun listSpacesAuthenticatingFirstWithError() {
-        underTest.credentials.token = ""
-        whenever(mockCoworkingMapService.getJwt(user, password)).thenReturn(Single.error(exception))
-        whenever(mockHttpErrorHandler.handle<Space>(exception)).thenReturn(Single.error(handledException))
+    fun isTokenNotExpired() {
+        val throwable = Exception()
 
-        val testObserver = underTest.listSpaces(city, space, country).test()
-        testObserver.awaitTerminalEvent()
+        val isTokenExpired = underTest.isTokenExpired(throwable)
 
-        testObserver.apply {
-            valueCount() shouldEqual 0
-            errorCount() shouldEqual 1
-            errors()[0] shouldEqual handledException
-        }
-
-        underTest.credentials.token shouldEqual ""
-        verify(mockCoworkingMapService, never()).listSpaces(any(), any())
-        verify(mockCoworkingMapService, never()).listSpaces(any(), any(), any())
-        verify(mockCoworkingMapService, never()).listSpaces(any(), any(), any(), any())
-    }
-
-    @Test
-    fun listSpacesWithExpiredToken() {
-        underTest.credentials.token = invalidToken
-        whenever(mockHttpErrorHandler.isTokenExpired(exception)).thenReturn(true)
-        whenever(mockCoworkingMapService.getJwt(underTest.credentials.user, underTest.credentials.password)).thenReturn(Single.just(jwt))
-
-        val result = underTest.listSpaces(city, space, country).blockingGet()
-
-        result shouldEqual spaces
-        underTest.credentials.token shouldEqual validToken
+        isTokenExpired shouldEqual false
     }
 
     @Test
@@ -144,4 +98,5 @@ class CoworkingMapApiTest {
 
         tokenForRequest shouldEqual expectedTokenForRequest
     }
+
 }
