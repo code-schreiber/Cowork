76ea1199f4fff3608839c80fddb8af3dc6790429
code-schreiber <code-schreiber@users.noreply.github.com>
2018-08-02 10:16:53 +0200

    Adds ErrorHandlerParameterizedTest

--- base_repository/src/main/java/com/toolslab/cowork/base_repository/ErrorHandler.kt
+++ base_repository/src/main/java/com/toolslab/cowork/base_repository/ErrorHandler.kt
@@ -1,6 +1,7 @@
 package com.toolslab.cowork.base_repository
 
 import com.toolslab.cowork.base_repository.exception.NoConnectionException
+import com.toolslab.cowork.base_repository.exception.RepositoryException
 import io.reactivex.Single
 import java.io.IOException
 import javax.inject.Inject
@@ -10,7 +11,7 @@ internal class ErrorHandler @Inject constructor() {
     internal fun <T> handle(throwable: Throwable): Single<T> =
             when (throwable) {
                 is IOException -> Single.error(NoConnectionException(throwable))
-                else -> Single.error(throwable)
+                else -> Single.error(RepositoryException(throwable))
             }
 
 }
