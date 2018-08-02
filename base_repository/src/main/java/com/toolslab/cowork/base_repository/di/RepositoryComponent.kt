60718b4aa5eada48b4f76ce79a2fc8920b45bc30
code-schreiber <code-schreiber@users.noreply.github.com>
2018-08-02 14:49:13 +0200

    Fixes network module

--- base_repository/src/main/java/com/toolslab/cowork/base_repository/di/RepositoryComponent.kt
+++ base_repository/src/main/java/com/toolslab/cowork/base_repository/di/RepositoryComponent.kt
@@ -1,9 +1,16 @@
 package com.toolslab.cowork.base_repository.di
 
+import com.toolslab.cowork.base_network.CoworkingMapService
+import com.toolslab.cowork.base_network.di.NetworkModule
 import dagger.Component
 
-@Component
+@Component(
+        modules = [NetworkModule::class]
+)
 interface RepositoryComponent {
+
+    // Provision method so CoworkingMapService gets provided to consumers of this component
+    fun provideCoworkingMapService(): CoworkingMapService
 
     @Component.Builder
     interface Builder {
