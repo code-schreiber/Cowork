92b9503013f739665988529f95cb796f560f946b
code-schreiber <code-schreiber@users.noreply.github.com>
2018-08-06 11:33:32 +0200

    Moves showMessage() to BaseActivity

--- app/src/main/java/com/toolslab/cowork/view/base/BaseActivity.kt
+++ app/src/main/java/com/toolslab/cowork/view/base/BaseActivity.kt
@@ -1,8 +1,15 @@
 package com.toolslab.cowork.view.base
 
-
 import android.annotation.SuppressLint
+import android.widget.Toast
+import com.toolslab.cowork.base_mvp.BaseView
 import dagger.android.support.DaggerAppCompatActivity
 
 @SuppressLint("Registered") // BaseActivity should not go in the manifest
-open class BaseActivity : DaggerAppCompatActivity()
+open class BaseActivity : DaggerAppCompatActivity(), BaseView {
+
+    override fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+}
--- base_mvp/src/main/java/com/toolslab/cowork/base_mvp/BaseView.kt
+++ base_mvp/src/main/java/com/toolslab/cowork/base_mvp/BaseView.kt
@@ -1,3 +1,5 @@
 package com.toolslab.cowork.base_mvp
 
-interface BaseView : MvpView
+interface BaseView : MvpView {
+    fun showMessage(message: String)
+}
